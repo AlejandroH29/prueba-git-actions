17057e2cd16b2523cd3cb8592f5ceddfd0449144
Dieguito <diego_a.hernandez@uao.edu.co>
2025-11-04 18:03:14 -0500

    Primer cambio

---
+++ src/_test_/index.test.js
@@ -0,0 +1,10 @@
+import { sum } from "..";
+
+describe("sum", () => {
+    test("should add two numbers correctly", () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+    test("should handle negative numbers", () => {
+        expect(sum(-5, 3)).toBe(-2);
+    }); 
+});
